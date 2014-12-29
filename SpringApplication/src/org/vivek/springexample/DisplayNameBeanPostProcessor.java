3309c3e11206c7145c6acf7fc419d23d389e10d4
sawantvivek <vivek.sawant1985@gmail.com>
2014-12-29 11:27:46 +0530

    https://github.com/sawantvivek/spring

---
+++ SpringApplication/src/org/vivek/springexample/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,24 @@
+package org.vivek.springexample;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("In after Initialization, bean name is " + beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out
+				.println("In before Initialization, bean name is " + beanName);
+		return bean;
+	}
+
+}
